3a3a4c6077415bef34a1461c74fb7c598e114a38
blinklv <blinklv@icloud.com>
2020-05-08 16:25:33 +0800

    feat: add ToJson

---
+++ codec.go
@@ -0,0 +1,24 @@
+// codec.go
+//
+// Author: blinklv <blinklv@icloud.com>
+// Create Time: 2020-05-08
+// Maintainer: blinklv <blinklv@icloud.com>
+// Last Change: 2020-05-08
+
+package util
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+// ToJson returns the JSON encoding of v. Compared with json.Marshal,
+// it won't escape special characters (&, <, and >) in quoted strings
+// to avoid certain safety problems, and doesn't return error.
+func ToJson(v interface{}) []byte {
+	b := &bytes.Buffer{}
+	enc := json.NewEncoder(b)
+	enc.SetEscapeHTML(false)
+	enc.Encode(v)
+	return b.Bytes()
+}
