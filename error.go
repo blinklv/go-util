4023d74874fda82aa41c92298dc77ea90d81dfaf
blinklv <blinklv@icloud.com>
2020-08-12 19:03:28 +0800

    feat: add ErrorFormatter/ListErrorFormatter

--- error.go
+++ error.go
@@ -3,13 +3,14 @@
 // Author: blinklv <blinklv@icloud.com>
 // Create Time: 2020-04-30
 // Maintainer: blinklv <blinklv@icloud.com>
-// Last Change: 2020-05-18
+// Last Change: 2020-08-12
 
 // Package package contains some utility functions and types.
 package util
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Error is an implementation of the error interface, and an additional
@@ -39,4 +40,36 @@ func Errorf(code int, format string, args ...interface{}) error {
 // type of the returned error is *util.Error.
 func WrapError(code int, err error) error {
 	return &Error{err, code}
+}
+
+// ErrorFormatter specifies an interface that can convert the list of errors into a string.
+type ErrorFormatter func([]error) string
+
+// ListErrorFormatter is a basic formatter that outputs the number of errors
+// in the form of a list. If there is only one error, returns the error itself.
+// If there're more than 16 errors, the remaining errors are indicated by ellipsis.
+func ListErrorFormatter(es []error) string {
+	if len(es) == 0 {
+		return "<nil>"
+	} else if len(es) == 1 {
+		return es[0].Error()
+	}
+
+	var b strings.Builder
+	b.WriteString(fmt.Sprintf("multiple (%d) errors:\n", len(es)))
+
+	for i, e := range es {
+		if i > 0 {
+			b.WriteString("\n")
+		}
+
+		if i >= 16 {
+			b.WriteString("      ...")
+			break
+		}
+
+		b.WriteString(fmt.Sprintf("%4d. %s", i+1, e))
+	}
+
+	return b.String()
 }
